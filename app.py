ac468c4a36dd07dde8972a3436bcf216d79ce6c3
ilya <27romashka05@gmail.com>
2022-12-08 18:45:57 +0500

    first version of footer, main and header

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/main')
 def index():
-    return "PetSeeker"
+    return render_template("index.html")
 
 
 @app.route('/posts')
 def posts():
-    return "Posts"
+    return render_template("posts.html")
+
+
+@app.route('/create/<string:id>')
+def create(id):
+    return "Create post " + id
 
 
 if __name__ == '__main__':
